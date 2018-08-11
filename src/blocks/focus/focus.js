d2ae1d1e293227ad9bde5c04b668a210f7a0ddd4
Maxim Sofronov <imaxsof@gmail.com>
2018-08-11 19:18:36 +0300

    fix: исправления по фокусу

--- src/blocks/focus/focus.js
+++ src/blocks/focus/focus.js
@@ -1,31 +1,32 @@
 // Focus
 //------------------------------------------------------------------------------
 
-(function($) {
-
-	Array.prototype.forEach.call(document.querySelectorAll('.js-choose-focus'), function(el) {
-		el.addEventListener('change', function(value) {
-			if (this.value === 'all') {
-				LIST_BOARD.removeClass('board__lists--today  board__lists--planned  board__lists--someday');
-
-				LIST_BOARD.addClass('board__lists--all');
-			} else if (this.value === 'today') {
-				LIST_BOARD.removeClass('board__lists--all  board__lists--planned  board__lists--someday');
-
-				LIST_BOARD.addClass('board__lists--today');
-			} else if (this.value === 'planned') {
-				LIST_BOARD.removeClass('board__lists--all  board__lists--today  board__lists--someday');
-
-				LIST_BOARD.addClass('board__lists--planned');
-			} else if (this.value === 'someday') {
-				LIST_BOARD.removeClass('board__lists--all  board__lists--today  board__lists--planned');
-
-				LIST_BOARD.addClass('board__lists--someday');
-			}
-		});
+Array.prototype.forEach.call(document.querySelectorAll('.js-choose-focus'), function(el) {
+	el.addEventListener('change', function(value) {
+		changeFocus(this.value);
 	});
+});
 
-	// Показываем содержимое доски
-	$('body').addClass('ready');
+function changeFocus(value) {
+	if (value === 'all') {
+		document.body.classList.remove('focus-today', 'focus-planned', 'focus-someday', 'focus-completed');
 
-}(jQuery));
+		document.body.classList.add('focus-all');
+	} else if (value === 'today') {
+		document.body.classList.remove('focus-all', 'focus-planned', 'focus-someday', 'focus-completed');
+
+		document.body.classList.add('focus-today');
+	} else if (value === 'planned') {
+		document.body.classList.remove('focus-all', 'focus-today', 'focus-someday', 'focus-completed');
+
+		document.body.classList.add('focus-planned');
+	} else if (value === 'someday') {
+		document.body.classList.remove('focus-all', 'focus-today', 'focus-planned', 'focus-completed');
+
+		document.body.classList.add('focus-someday');
+	} else if (value === 'completed') {
+		document.body.classList.remove('focus-all', 'focus-today', 'focus-planned', 'focus-someday');
+
+		document.body.classList.add('focus-completed');
+	}
+}
